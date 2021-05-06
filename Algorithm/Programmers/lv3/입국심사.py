af2cbe117f099d083be6d3fc01761411ca6d8d17
Hyunta <mohai2618@naver.com>
2021-05-06 16:55:42 +0900

    210505 알고리즘 풀이

---
+++ Algorithm/Programmers/lv3/입국심사.py
@@ -0,0 +1,19 @@
+def solution(n, times):
+    answer = 0
+    left = 1
+    right = n * max(times)
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        cnt = 0
+        for t in times:
+            cnt += mid // t
+
+        if cnt >= n:
+            answer = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return answer
