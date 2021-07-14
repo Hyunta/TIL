98a56662b124b701e8652e8c55a254c6d8af9ac4
Hyunta <mohai2618@naver.com>
2021-07-14 11:33:55 +0900

    210714 수정

--- Algorithm/Test/2021 카카오페이/4.py
+++ Algorithm/Test/2021 카카오페이/4.py
@@ -17,15 +17,15 @@ def solution(ages, wires):
 
     days = min(ages)
 
-    while sum(list(x for y in time[x][1]):
-        res = 100000000
-        for i in range(len(time)):
-            time[i][1] -= days
-            if time[i][1] < 0:
-                time[i][1] = 0
-            if time[i][1] < res and time[i][1] > 0:
-                res = time[i][1]
-        days = res
-        print(time)
+    # while sum(list(x for y in time[x][1]):
+    #     res = 100000000
+    #     for i in range(len(time)):
+    #         time[i][1] -= days
+    #         if time[i][1] < 0:
+    #             time[i][1] = 0
+    #         if time[i][1] < res and time[i][1] > 0:
+    #             res = time[i][1]
+    #     days = res
+    #     print(time)
 
 print(solution([35,25,3,8,7], [[1,2,5],[2,1,5],[1,3,2],[3,4,2],[3,5,20],[4,5,1]]))
