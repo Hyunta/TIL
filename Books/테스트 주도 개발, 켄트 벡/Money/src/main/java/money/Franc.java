ac8c97fea0159667b81fe8d9d7a258d91b39f3f1
Hyunta <mohai2618@naver.com>
2021-07-27 17:59:03 +0900

    210725 [TDD] 8장 객체 만들기

--- Books/테스트 주도 개발, 켄트 벡/Money/src/main/java/money/Franc.java
+++ Books/테스트 주도 개발, 켄트 벡/Money/src/main/java/money/Franc.java
@@ -1,18 +1,14 @@
 package money;
 
-public class Franc {
-    private int amount;
+public class Franc extends Money{
 
     public Franc(int amount) {
         this.amount = amount;
     }
 
-    Franc times(int multiplier) {
+    Money times(int multiplier) {
         return new Franc(amount * multiplier);
     }
 
-    public boolean equals(Object object) {
-        Franc franc = (Franc) object;
-        return amount == franc.amount;
-    }
+
 }
