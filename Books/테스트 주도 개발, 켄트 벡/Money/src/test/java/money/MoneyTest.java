34da41eca16adc8d6075c238da4b9847f2ba416c
Hyunta <mohai2618@naver.com>
2021-07-30 22:55:48 +0900

    210725 [TDD] 11장 모든 악의 근원

--- Books/테스트 주도 개발, 켄트 벡/Money/src/test/java/money/MoneyTest.java
+++ Books/테스트 주도 개발, 켄트 벡/Money/src/test/java/money/MoneyTest.java
@@ -7,14 +7,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class MoneyTest {
 
     @Test
-    public void testMuliplication() {
+    public void testMultiplication() {
         Money five = Money.dollar(5);
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
-    public void testFrancMuliplication() {
+    public void testFrancMultiplication() {
         Money five = Money.franc(5);
         assertEquals(Money.franc(10), five.times(2));
         assertEquals(Money.franc(15), five.times(3));
@@ -24,8 +24,6 @@ class MoneyTest {
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
@@ -33,12 +31,6 @@ class MoneyTest {
     public void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
-    }
-
-    @Test
-    public void testDifferentClassEquality() {
-        assertTrue(new Money(10, "CHF").equals(
-                new Franc(10, "CHF")));
     }
 
 }
