9154f1043b13ce1599a85f217cbc0daa8bad1bdd
Hyunta <mohai2618@naver.com>
2021-08-08 16:55:20 +0900

    210808 19장. 테이블 차리기

--- Books/테스트 주도 개발, 켄트 벡/xUnit/xUnit.py
+++ Books/테스트 주도 개발, 켄트 벡/xUnit/xUnit.py
@@ -1,23 +1,53 @@
 class TestCase:
     def __init__(self, name):
+        print("TestCase1")
+        print("name= ", name)
         self.name = name
 
+    def setUp(self):
+        print("TestCase2")
+        pass
+
     def run(self):
+        print("TestCase3")
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
-class wasRun(TestCase):
+
+class WasRun(TestCase):
     def __init__(self, name):
+        print("WasRun1")
         self.wasRun= None
         TestCase.__init__(self, name)
+
     def testMethod(self):
+        print("WasRun2")
         self.wasRun = 1
 
+    def setUp(self):
+        print("WasRun3")
+        self.wasRun = None
+        self.wasSetUp = 1
+
+
 class TestCaseTest(TestCase):
+    def setUp(self):
+        print("TestCaseTest1")
+        self.test = WasRun("testMethod")
+
     def testRunning(self):
-        test = wasRun("testMethod")
-        assert (not test.wasRun)
-        test.run()
-        assert (test.wasRun)
+        print("TestCaseTest2")
+        self.test.run()
+        assert (self.test.wasRun)
+        print(self.test.wasRun)
+        print(str(self))
+
+    def testSetUp(self):
+        print("TestCaseTest3")
+        self.test.run()
+        assert (self.test.wasSetUp)
 
 TestCaseTest("testRunning").run()
+print("------"*50)
+TestCaseTest("testSetUp").run()
